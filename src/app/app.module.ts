577921954cb2b5ca97c7f387fb37a8095798267e
Jah Markabawi <jmarkaba@asu.edu>
2020-04-06 15:31:52 -0700

    added title and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { CharacterSheetModule } from './character-sheet/character-sheet.module';
 import { GameBoardModule } from './game-board/game-board.module';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent, ClickStopPropagation } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FrontPageComponent,
+    ClickStopPropagation
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
+    MDBBootstrapModule.forRoot(),
     CharacterSheetModule,
     GameBoardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, FrontPageComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -14,7 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatExpansionModule,
     MatTabsModule,
     MatTooltipModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [
     MatIconRegistry
@@ -25,7 +33,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatExpansionModule,
     MatTooltipModule,
     MatTabsModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule {
